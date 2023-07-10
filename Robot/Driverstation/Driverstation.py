c097ba5ba5b73dddb6e043ffa28bc59d31b4748e
tomerBARZ <tomerminecraft9@gmail.com>
2023-07-10 18:31:03 +0300

    Driverstation gui colors

--- Robot/Driverstation/Driverstation.py
+++ Robot/Driverstation/Driverstation.py
@@ -23,7 +23,7 @@ def disable_robot(client_socket, window):
     window.configure(bg='red')
     send_message(client_socket, "disable")
 
-def connect_to_server(host, port):
+def connect_to_server(host, port,window):
     global connected,client_socket
     try:
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -31,10 +31,12 @@ def connect_to_server(host, port):
         client_socket.connect((host, port))
         connected = True
         print('Connected to the robot.')
+        window.configure(bg='red')
 
     except ConnectionRefusedError:
         connected = False
         print('Connection refused. Make sure the robot is running.')
+        window.configure(bg='grey')
 
 def main():
     global connected,client_socket, enabled
@@ -53,7 +55,7 @@ def main():
         
         # Send data to the server
         msg = pickle.dumps(controller.getValues())
-        print('sent -',msg)
+        # print('sent -',msg)
         client_socket.send(msg)
         window.after(pollingRateMs, loop)
 
@@ -61,25 +63,25 @@ def main():
     window = tk.Tk()
     window.title("Robot Controller")
     enabled = False
-    window.configure(bg='red')
+    window.configure(bg='grey')
+    window.resizable(False, False)
+    window.geometry("100x75")
 
     # Connect button
-    connect_button = tk.Button(window, text="Connect", command=lambda: connect_to_server(host, port))
-    connect_button.pack()
+    connect_button = tk.Button(window, text="Connect", command=lambda: connect_to_server(host, port,window),width=10).pack()
 
     # Enable button
-    enable_button = tk.Button(window, text="Enable", command=lambda: enable_robot(client_socket,window))
-    enable_button.pack()
+    enable_button = tk.Button(window, text="Enable", command=lambda: enable_robot(client_socket,window),width=10).pack()
 
     # Disable button
-    disable_button = tk.Button(window, text="Disable", command=lambda: disable_robot(client_socket,window))
-    disable_button.pack()
+    disable_button = tk.Button(window, text="Disable", command=lambda: disable_robot(client_socket,window),width=10).pack()
 
     window.after(pollingRateMs, loop)
     window.mainloop()
 
     # Close the socket
     client_socket.close()
+    window.configure(bg='grey')
     print('Socket closed.')
 
 if __name__ == '__main__':
